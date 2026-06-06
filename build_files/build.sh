d883cc3f909f71e214fe8d95e6e418a72418ed9d
Gabriele La Greca <gabriele.lagreca@gmail.com>
2026-06-06 21:29:54 +0200

    feat: working on it

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,17 +9,39 @@ sed -i '/^\[main\]/a max_parallel_downloads=10' /etc/dnf/dnf.conf
 ### Install packages
 
 ### BASE PACKAGES
-dnf -y install \
-    fish 
+dnf5.real -y install \
+    fish \
+    vim \
+    neovim \
+    firefox \
+    nautilus \
+    file-roller \
+    loupe \
+    totem
+
+dnf5.real -y install \
+    kitty \
+    fastfetch \
+    lolcat lsd bat fzf 
+
+# Nautilus open any terminal extension
+curl -Lo /etc/yum.repos.d/nautilus-open-any-terminal.repo \
+  https://copr.fedorainfracloud.org/coprs/monkeygold/nautilus-open-any-terminal/repo/fedora-$(rpm -E %fedora)/monkeygold-nautilus-open-any-terminal-fedora-$(rpm -E %fedora).repo
+
+dnf5.real install -y \
+    nautilus-open-any-terminal
+
+glib-compile-schemas /usr/share/glib-2.0/schemas
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty
 
 ### DESKTOP ENVIRONMENT
 
 # Niri
-dnf -y install niri
+dnf5.real -y install niri bibata-cursor-theme
 
 # Dank Linux Shell
 sudo curl --output-dir "/etc/yum.repos.d/" --remote-name "https://copr.fedorainfracloud.org/coprs/avengemedia/dms/repo/fedora-$(rpm -E %fedora)/avengemedia-dms-fedora-$(rpm -E %fedora).repo"
-dnf -y install quickshell dms greetd dms-greeter --allowerasing 
+dnf5.real -y install quickshell dms greetd dms-greeter --allowerasing 
 
 mkdir -p /etc/greetd/
 cat > /etc/greetd/config.toml << EOF
@@ -33,6 +55,20 @@ EOF
 rm -f /etc/systemd/system/display-manager.service
 ln -s /usr/lib/systemd/system/greetd.service /etc/systemd/system/display-manager.service
 systemctl enable --force greetd.service
+
+mkdir -p /etc/skel/.config/systemd/user/graphical-session.target.wants
+ln -s /usr/lib/systemd/user/dms.service /etc/skel/.config/systemd/user/graphical-session.target.wants/
+
+## USER FILES (DOT FILES)
+mkdir -p /etc/skel/.config
+cp -rf /ctx/dot_config/* /etc/skel/.config/
+
+# Neovim
+git clone --depth 1 https://github.com/AstroNvim/template /etc/skel/.config/nvim
+rm -rf /etc/skel/.config/nvim/.git
+
+dnf5.real -y remove \
+    waybar
 
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -54,7 +90,16 @@ systemctl enable --force greetd.service
 
 systemctl enable podman.socket
 
+# Plymouth
+plymouth-set-default-theme spinner
+
+# mkdir -p /usr/lib/bootc/kargs.d/
+# cat > /usr/lib/bootc/kargs.d/00-splash.toml << 'EOF'
+# kargs = ["quiet", "rhgb"]
+# EOF
+
 ## CLEAN UP
 dnf5 -y clean all
 rm -rf /run/dnf /run/selinux-policy
 rm -rf /var/lib/dnf
+
---
+++ build_files/dot_config/fish/functions/v.fish
@@ -0,0 +1,4 @@
+function v --wraps=nvim --wraps=/home/gabriele97/Bin/neovim.appimage --description 'alias v=/home/gabriele97/Bin/neovim.appimage'
+  nvim $argv
+        
+end
