cf422d0f3123c91f82754986f0ba98f5f6908b9b
Gabriele La Greca <gabriele.lagreca@gmail.com>
2026-06-18 16:40:14 +0200

    feat (image): added fastfetch system config

---
+++ system_files/usr/share/gabos/gdots/dot_config/fish/functions/fastfetch.fish
@@ -0,0 +1,3 @@
+function fastfetch --wraps=fastfetch --description 'alias fastfetch fastfetch --config /usr/share/gabos/gdots/system/fastfetch/fastfetch.jsonc'
+  fastfetch --config /usr/share/gabos/gdots/system/fastfetch/fastfetch.jsonc $argv
+end
